d82679d6a9b3be65fd328b82691fc5b3c6b4d85a
reason595 <kiaff450@gmail.com>
2026-09-07 14:27:21 +0000

    Update h.c

--- h.c
+++ h.c
@@ -1,3 +1,5 @@
 https://github.com/bonigarcia/c-programming.git 
+https://github.com/Deepali-Srivastava/data-structures-and-algorithms-in-c.git
+https://github.com/MyNameIsGustavo/DataStructuresAndAlgorithms.git
 curl -fsSL https://code.kimi.com/kimi-code/install.sh | bash
 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
