e9c72ef5914cd56b1e6996731090dcb12b9fbaaf
reason595 <kiaff450@gmail.com>
2026-09-07 14:11:56 +0000

    Create h.c

---
+++ h.c
@@ -0,0 +1,3 @@
+https://github.com/bonigarcia/c-programming.git 
+curl -fsSL https://code.kimi.com/kimi-code/install.sh | bash
+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
